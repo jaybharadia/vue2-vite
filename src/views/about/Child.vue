83d454631cddf41587168dc01e85ab97b28aa979
jaybharadia <jay.p.bharadia@gmail.com>
2023-09-04 10:09:26 +0530

    checking #1 commit

--- src/views/about/Child.vue
+++ src/views/about/Child.vue
@@ -1,9 +1,10 @@
 <template>
-  <div>About Child Page Query params : {{ query }}</div>
+    <div>About Child Page Query params : {{ query }}</div>
 </template>
 
 <script>
+console.log("checking rebase #1 commit");
 export default {
-  props: ["query"],
+    props: ["query"],
 };
 </script>
